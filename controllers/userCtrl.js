ee1570a805996c853305d519407a7d54b4d4d767
Anjani <anjanison9@gmail.com>
2023-06-19 15:39:35 +0530

    User_Login_done

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,8 +1,25 @@
 const userModel = require('../models/userModels')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+//  login callback
+const loginController = async(req,res) => {
+     try {
+        const user = await userModel.findOne({email:req.body.email})
+        if(!user){
+            return res.status(200).send({message:`user not found`,success:false})
+        }
+        const isMatch = await bcrypt.compare(req.body.password,user.password)
+        if(!isMatch){
+            return res.status(200).send({message:`Invalid Credentials`,success:false})
+        }
 
-
-const loginController = () => {}
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"})
+        res.status(200).send({message:`Login Success`,success:true,token})
+     } catch (error) {
+        console.log(error);
+        res.status(500).send({message:`Error in Login CTRL ${error.message}`})
+     }
+}
 
 // register callback
 const registerController = async(req,res) => {
